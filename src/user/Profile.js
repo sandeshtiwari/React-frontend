02d7e70d819602da6a08df60c180b353c29f9a9c
Sandesh Tiwari <sandeshtiwari@live.com>
2019-05-14 14:29:11 -0500

    refactor: Destructure the object to make things simpler

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {isAuthenticated} from '../auth';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
 import {read} from './apiUser';
 
 class Profile extends Component {
@@ -34,7 +34,7 @@ class Profile extends Component {
   }
 
   render() {
-    const redirectToSignin = this.state.redirectToSignin;
+    const {redirectToSignin, user} = this.state;
     if(redirectToSignin){
       return(
         <Redirect to="/signin" />
@@ -42,10 +42,26 @@ class Profile extends Component {
     }
     return(
       <div className='container'>
-        <h2 className='mt-5 mb-5'>Profile</h2>
-        <p>Hello {isAuthenticated().user.name}</p>
-        <p>Email: {isAuthenticated().user.email}</p>
-        <p>{`Joined on ${new Date(this.state.user.created).toDateString()}`}</p>
+        <div className="row">
+          <div className="col-md-6">
+            <h2 className='mt-5 mb-5'>Profile</h2>
+            <p>Hello {isAuthenticated().user.name}</p>
+            <p>Email: {isAuthenticated().user.email}</p>
+            <p>{`Joined on ${new Date(user.created).toDateString()}`}</p>
+          </div>
+          <div className="col-md-6">
+            {isAuthenticated().user && isAuthenticated().user._id == user._id && (
+              <div className="d-inline-block mt-5">
+                <Link className="btn btn-raised btn-success mr-5" to={`/user/edit/${user._id}`}>
+                  Edit Profile
+                </Link>
+                <Link className="btn btn-raised btn-danger mr-5" to={`/user/delete/${user._id}`}>
+                  Delete Profile
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
