294c416d9056caa4eed8499c80d6ef86f2c6763b
Sandesh Tiwari <sandeshtiwari@live.com>
2019-05-14 11:33:02 -0500

    refactor: Refactor code for better management

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {isAuthenticated} from '../auth';
 import {Redirect} from 'react-router-dom';
+import {read} from './apiUser';
 
 class Profile extends Component {
 
@@ -12,19 +13,11 @@ class Profile extends Component {
     }
   }
 
-  componentDidMount(){
-    const userId = this.props.match.params.userId;
-    fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${isAuthenticated().token}`
-      }
-    })
-    .then(response => {
-      return response.json();
-    })
+
+
+  init = (userId) => {
+    const token = isAuthenticated().token;
+    read(userId, token)
     .then(data => {
       if(data.error){
         this.setState({redirectToSignin: true});
@@ -33,6 +26,11 @@ class Profile extends Component {
         this.setState({user: data});
       }
     })
+  }
+
+  componentDidMount(){
+    const userId = this.props.match.params.userId;
+    this.init(userId);
   }
 
   render() {
