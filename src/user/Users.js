e164a5df4c889c633b7f7b3e73065943cb346b16
Sandesh Tiwari <sandeshtiwari@live.com>
2019-05-14 14:51:18 -0500

    feat: Request users from the database

--- src/user/Users.js
+++ src/user/Users.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react';
+import {list} from './apiUser';
 
 class User extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    list().then(data => {
+      if(data.error){
+        console.log(data.error);
+      }
+      else{
+        this.setState({users: data});
+      }
+    });
+  }
+
   render() {
     return(
       <div className="container">
