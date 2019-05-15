fa720423a4a6d22c42ae52c281aefe6ec1c04407
Sandesh Tiwari <sandeshtiwari@live.com>
2019-05-15 17:22:34 -0500

    feat: Implement edit profile functionality

--- src/user/EditProfile.js
+++ src/user/EditProfile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {isAuthenticated} from '../auth';
-import {read} from "./apiUser";
+import {read, update} from "./apiUser";
+import {Redirect} from 'react-router-dom';
 
 class EditProfile extends Component {
 
@@ -10,7 +11,8 @@ class EditProfile extends Component {
       id: "",
       name:"",
       email: "",
-      password:""
+      password:"",
+      redirectToProfile: false
     }
   }
 
@@ -22,7 +24,12 @@ class EditProfile extends Component {
         this.setState({redirectToSignin:true});
       }
       else{
-        this.setState({id: data._id, name: data.name, email:data.email});
+        this.setState({
+          id: data._id,
+          name: data.name,
+          email:data.email,
+          error:  ''
+        });
       }
     })
   }
@@ -42,25 +49,22 @@ class EditProfile extends Component {
     const user = {
       name: name,
       email: email,
-      password: password
+      password: password || undefined
     };
-    console.log(user);
     // console.log(user);
-    // signup(user)
-    // .then((data) => {
-    //   if(data.error){
-    //     this.setState({error: data.error});
-    //   }
-    //   else{
-    //     this.setState({
-    //       name: "",
-    //       email: "",
-    //       password: "",
-    //       error: "",
-    //       open: true
-    //     })
-    //   }
-    // })
+    const userId = this.props.match.params.userId;
+    const token = isAuthenticated().token;
+    update(userId, token, user)
+    .then((data) => {
+      if(data.error){
+        this.setState({error: data.error});
+      }
+      else{
+        this.setState({
+          redirectToProfile: true
+        })
+      }
+    })
   }
 
   signupForm = (name, email, password) => (
@@ -99,7 +103,10 @@ class EditProfile extends Component {
   )
 
   render() {
-    const {name, email, password} = this.state;
+    const {id, name, email, password, redirectToProfile} = this.state;
+    if(redirectToProfile){
+      return <Redirect to={`/user/${id}`} />
+    }
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Edit Profile</h2>
